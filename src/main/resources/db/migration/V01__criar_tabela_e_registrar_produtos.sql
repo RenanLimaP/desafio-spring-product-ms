498730384e020d11ef334500f4c13fba88739774
Renan Lima <renan.lima@portoseguro.com.br>
2021-03-12 20:45:32 -0300

    Corrige sql

--- src/main/resources/db/migration/V01__criar_tabela_e_registrar_produtos.sql
+++ src/main/resources/db/migration/V01__criar_tabela_e_registrar_produtos.sql
@@ -2,8 +2,8 @@ CREATE TABLE produto (
 	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
 	name VARCHAR(50) NOT NULL,
 	description VARCHAR(30),
-	price DECIMAL(10,2) NOT NULL,
+	price DECIMAL(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO produto (id, name, description, price) values ('Televisão', 'Smart TV 50 polegadas', 2.000);
-INSERT INTO produto (id, name, description, price) values ('SmartPhone', 'Samsung', 1.000);
+INSERT INTO produto (name, description, price) values ('Televisão', 'Smart TV 50 polegadas', 2000);
+INSERT INTO produto (name, description, price) values ('SmartPhone', 'Samsung', 1000);
