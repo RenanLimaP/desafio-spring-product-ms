539927c7c90f704dab1fce6453f63574330fad8f
Renan Lima <renan.lima@portoseguro.com.br>
2021-03-12 20:20:21 -0300

    Adiciona configurações Heroku e anotações JPA

--- src/main/java/com/desafio/spring/model/Produto.java
+++ src/main/java/com/desafio/spring/model/Produto.java
@@ -4,6 +4,11 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -13,9 +18,17 @@ public class Produto implements Serializable {
 
 	private static final long serialVersionUID = -3569116723556332181L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+
+	@NotBlank(message = "Nome inválido")
 	private String name;
+
+	@NotBlank(message = "Descrição inválida")
 	private String description;
+
+	@NotNull(message = "Preço inválido")
 	private BigDecimal price;
 
 }
---
+++ src/main/resources/db/migration/V01__criar_tabela_e_registrar_produtos.sql
@@ -0,0 +1,9 @@
+CREATE TABLE produto (
+	id BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	description VARCHAR(30),
+	price DECIMAL(10,2) NOT NULL,
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO produto (id, name, description, price) values ('Televisão', 'Smart TV 50 polegadas', 2.000);
+INSERT INTO produto (id, name, description, price) values ('SmartPhone', 'Samsung', 1.000);
